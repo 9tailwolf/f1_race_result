2c6dd07093b9e20ef4dca1c42929dd2fb4604597
9tailwolf <81214756+9tailwolf@users.noreply.github.com>
2024-02-04 21:24:15 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect} from "react";
 import ChartRace from "./ChartRace.js";
 import './App.css';
 
@@ -6,27 +6,26 @@ import jsondata from "./data/2023Bahrain.json"
 
 function App() {
   const [data, setData] = useState([]);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(1);
+  const [isActive, setIsActive] = useState(false);
+  const [absoluteActive, setAbsoluteActive] = useState(true);
+  
+
   function getData(index) {
     return jsondata.data[index].data;
   }
 
   function getLap(index) {
-    return jsondata.data[index].lap;
+    return jsondata.data[index-1].lap;
   }
 
-  // read data function
-  
-
   function handleChange() {
-    setTimeout(() => {
-      setIndex(index + 1);
-    }, 200
+    setIndex(index + 1);
 
-    )
     const data = getData(index);
 
     setData([...data]);
+    
   }
 
   useEffect(() => {
@@ -35,22 +34,57 @@ function App() {
     setData([...data]);
   }, []);
 
+  useEffect(() => {
+    let id;
+
+    if (isActive && absoluteActive){
+    id = setInterval(() => {
+      handleChange();
+    }, 300);
+  }
+    else{
+      clearInterval(index);
+    }
+    
+    if(index === jsondata.end - 1) {
+      setAbsoluteActive(false);
+    }
+    return () => clearInterval(id);
+  }, [isActive, index]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handlePause = () => {
+    setIsActive(false);
+  };
+
+  const handleReset = () => {
+    setIndex(1);
+    setAbsoluteActive(true);
+    setIsActive(false);
+  };
+
   return (
+
     <div class='inner_div'>
-      <button onClick={handleChange}>Click Me!</button>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handlePause}>Pause</button>
+      <button onClick={handleReset}>Reset</button>
       <h1>{getLap(index)}</h1>
 
       <ChartRace
         data={data}
-        backgroundColor="#000"
+        backgroundColor="#ffffff"
         width={760}
         padding={12}
         itemHeight={20}
         gap={12}
-        titleStyle={{ font: "normal 400 13px Arial", color: "#fff" }}
+        titleStyle={{ font: "normal 400 13px Arial", color: "#000" }}
         valueStyle={{
           font: "normal 400 11px Arial",
-          color: "rgba(255,255,255, 0.42)"
+          color: "rgba(0,0,0, 0.42)"
         }}
       />
 
